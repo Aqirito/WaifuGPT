798bb129d718ff8744d3c2f87b0ed0df03c94f79
Aqirito <aqil.rahmat99@gmail.com>
2023-04-02 20:34:06 +0800

    UPDATE: implement CoquiTTS in flask api

--- backend/app/api/waifu.py
+++ backend/app/api/waifu.py
@@ -1,12 +1,22 @@
-from flask import (Blueprint, request, url_for, abort)
+from flask import (Blueprint, request, url_for, abort, send_file)
+from TTS.api import TTS
+import os
+model_name = TTS.list_models()[12]
+# Init TTS
+tts = TTS(model_name)
+
+def Synthesize(message):
+    tts.tts_to_file(text=message, file_path=os.path.join('app/api/output.wav'))
 
 bp = Blueprint('waifu', __name__, url_prefix='/api/waifu')
 
 @bp.post('/chats')
 def chats():
     try:
-        payload = request.get_json()
-        return payload
+        params = request.get_json()
+        messageText = params.get('messageText')
+        Synthesize(messageText)
+        return send_file(os.path.join('api/output.wav'), mimetype='audio/wav', as_attachment=False)
     
     except BaseException as err:
         return abort(400, err)
