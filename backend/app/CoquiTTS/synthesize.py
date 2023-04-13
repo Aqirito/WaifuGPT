95c97fff167fdb97deaa75662d91a1f76b78f576
Aqirito <aqil.rahmat99@gmail.com>
2023-04-13 08:10:00 +0800

    UPDATE: remove CoquiTTS because of too slow in my pc

--- backend/app/CoquiTTS/synthesize.py
+++
@@ -1,9 +0,0 @@
-from TTS.api import TTS
-
-model_name = TTS.list_models()[12]
-
-# Init TTS
-tts = TTS(model_name)
-
-def Synthesize(message):
-    tts.tts_to_file(text=message, speaker=tts.speakers[1], language=tts.languages[0], file_path="output.wav")
