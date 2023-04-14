ecb5116277979b6ec6d22202dfe3b780b3a3431c
Aqirito <aqil.rahmat99@gmail.com>
2023-04-14 21:01:07 +0800

    UPDATE: remove silly functions to return synthesized voice

--- backend/app/voicevox_api/voicevox_engine_api.py
+++ backend/app/voicevox_api/voicevox_engine_api.py
@@ -32,66 +32,45 @@ def textInput(speaker: str, text: str):
 
     params = {'speaker': speaker, 'text': translated_text}
 
-    response = requests.post(url + 'audio_query', params=params)
+    audio_query_response = requests.post(url + 'audio_query', params=params)
 
-    if response.status_code == 200:
-        query_data = json.loads(response.content.decode('utf-8'))
+    if audio_query_response.status_code == 200:
+        query_data = json.loads(audio_query_response.content.decode('utf-8'))
         kana_text = query_data['kana']
         if kana_text:
             print("_"*50+"kana"+"_"*50)
             print(kana_text)
-            accentPhrases(query_data, kana_text, speaker=speaker)
+            # accentPhrases(query_data, kana_text, speaker=speaker)
+            params = {'speaker': speaker, 'is_kana': 'true', 'text': kana_text}
+
+            accent_phrases_response = requests.post(url + 'accent_phrases', params=params)
+            if accent_phrases_response.status_code == 200:
+                new_phrases_str = accent_phrases_response.content.decode('utf-8')
+                query_data_str = json.dumps(query_data)
+                # replaceOldContent(new_phrases_str, query_data_str, speaker=kwargs['speaker'])
+                new_query_data = re.sub(r'\[{.*}\]', new_phrases_str, query_data_str)
+                """
+                The regular expression r'\[{.*}\]' matches a string that begins with [{ and ends with }], and may contain any characters in between.
+                The re.sub() function takes three arguments: the regular expression to match, the replacement string, and the input string.
+                In this case, we use the contents of the newphrases.json file as the replacement string,
+                and the contents of the query.json file as the input string.
+                Finally, we write the updated data to the newquery.json file.
+                """
+                if new_query_data:
+                    # synthesis(new_query_data, speaker=kwargs['speaker'])
+                    params = {'speaker': speaker}
+                    headers = {'Content-Type': 'application/json'}
+
+                    synthesis_response = requests.post(
+                        url + 'synthesis', params=params, headers=headers, data=new_query_data.encode('utf-8'))
+                    # with open(project_path + '\\voicevox_api\\audio.wav', 'wb') as f:
+                    #     f.write(response.content)
+                    return synthesis_response.content
+                else:
+                    print("Error: Could not find pattern in query_data")
+            else:
+                print(accent_phrases_response.content)
         else:
             print("_"*50+"kana not found"+"_"*50)
     else:
-        print(response.content)
-
-def accentPhrases(query_data: dict, kana_text: str, **kwargs):
-    assert isinstance(query_data, dict), 'query_data must be a dictionary'
-    assert isinstance(kana_text, str), 'kana_text must be a string'
-
-    params = {'speaker': kwargs['speaker'], 'is_kana': 'true', 'text': kana_text}
-
-    response = requests.post(url + 'accent_phrases', params=params)
-    if response.status_code == 200:
-        new_phrases_str = response.content.decode('utf-8')
-        query_data_str = json.dumps(query_data)
-        replaceOldContent(new_phrases_str, query_data_str, speaker=kwargs['speaker'])
-    else:
-        print(response.content)
-
-def replaceOldContent(new_phrases_str, query_data_str, **kwargs):
-    """
-    In this example, we use the re.sub() function to replace the contents of the query.
-    json file with the contents of the newphrases.json file.
-    The regular expression r'\[{.*}\]' matches a string that begins with [{ and ends with }], and may contain any characters in between.
-    The re.sub() function takes three arguments: the regular expression to match, the replacement string, and the input string.
-    In this case, we use the contents of the newphrases.json file as the replacement string,
-    and the contents of the query.json file as the input string.
-    Finally, we write the updated data to the newquery.json file.
-    """
-
-    new_query_data = re.sub(r'\[{.*}\]', new_phrases_str, query_data_str)
-    if new_query_data:
-        synthesis(new_query_data, speaker=kwargs['speaker'])
-    else:
-        print("Error: Could not find pattern in query_data")
-
-
-def synthesis(new_query_data, **kwargs):
-    assert isinstance(new_query_data, str), 'new_query_data must be a string'
-
-    params = {'speaker': kwargs['speaker']}
-    headers = {'Content-Type': 'application/json'}
-
-    response = requests.post(url + 'synthesis', params=params, headers=headers, data=new_query_data.encode('utf-8'))
-
-    with open(project_path + '\\voicevox_api\\audio.wav', 'wb') as f:
-        f.write(response.content)
-
-
-# if __name__=='__main__':
-#     textInput(
-#         text="hello, my name is sagiri.",
-#         speaker="37"
-#     )
+        print(audio_query_response.content)
